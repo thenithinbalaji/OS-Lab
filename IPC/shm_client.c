6206a80a8b8b40d04767f6642c310135d979b069
Gokulram A <gokulram2010418@ssn.edu.in>
2022-12-14 00:22:50 +0530

    added shared memory program

---
+++ IPC/shm_client.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <sys/types.h>
+#define SHMSZ 27
+
+int main()
+{
+    key_t key = 6002;
+    int shmid;
+    char *s, *shm;
+    if ((shmid = shmget(key, 27, 0666)) < 0)
+    {
+        perror("shmgeterror\n");
+    }
+    if ((shm = shmat(shmid, NULL, 0)) == (char *)-1)
+    {
+        perror("shmat\n");
+    }
+    // s=shm;
+    // printf("%s",*shm);
+     for (s = shm; *s != NULL; s++)
+    {
+        putchar(*s);
+    }
+
+    *shm = '*';
+    exit(0);
+}
---
+++ IPC/shm_server.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <sys/types.h>
+#define SHMSZ 27
+
+int main()
+{
+    key_t key = 6002;
+    int shmid;
+    char *s, *shm;
+    if ((shmid = shmget(key, 27, IPC_CREAT | 0666)) < 0)
+    {
+        perror("shmgeterror\n");
+    }
+    if ((shm = shmat(shmid, NULL, 0)) == (char *)-1)
+    {
+        perror("shmat\n");
+    }
+    s = shm;
+    printf("Enter the string to be shared :");
+    while (*s != '\n')
+    {
+        *s = getchar();
+        s++;
+    }
+
+    *s = NULL;
+    // *shm = NULL;
+    while (*shm != '*')
+    {
+        sleep(1);
+    }
+    exit(0);
+    return 0;
+}
